9d072c3818926c446353a942efaa2e2d0d30072e
Wenyu.Li <lwylwy@outlook.com>
2023-10-11 10:13:37 +0000

    send all images in group rather then save

--- tg.py
+++ tg.py
@@ -1,8 +1,8 @@
 import argparse
 import os
-import random
 
 import telebot
+from telebot.types import InputMediaPhoto
 from BingImageCreator import ImageGen
 
 if __name__ == "__main__":
@@ -28,12 +28,13 @@ if __name__ == "__main__":
         i = ImageGen(bing_cookie)
         bot.reply_to(message, "Using bing DALL-E 3 generating images please wait")
         images = i.get_images(s)
-        i.save_images(images, path)
-        images_count = len(list(os.listdir(path)))
-        print(images_count)
-        index = random.randint(images_count - len(images), images_count - 1)
-        with open(os.path.join(path, str(index)) + ".jpeg", "rb") as f:
-            bot.send_photo(message.chat.id, f, reply_to_message_id=message.message_id)
+        medias = []
+        for img in images:
+            medias.append(InputMediaPhoto(img))
+        bot.send_media_group(message.chat.id,
+                             media=medias,
+                             reply_to_message_id=message.message_id)
+
         return
 
     @bot.message_handler(func=reply_dalle_image)
